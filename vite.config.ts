d508a24454c600ff1eb9377d04dc99c915d85add
nisha-apiplatform <nisha@apiplatform.io>
2024-08-15 13:15:02 +0530

    teamp

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from "vite";
-import path from "path";
-import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  build: {
+    rollupOptions: {},
   },
 });
